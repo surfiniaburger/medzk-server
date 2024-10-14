1b74bac292592428cd0610e7a69b325fbc5f14ce
surfiniaburger <jdmasciano2@gmail.com>
2024-10-14 15:10:41 +0100

    changed server port to 8080

--- mern/server/server.js
+++ mern/server/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import records from "./routes/record.js";
 
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(cors());
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/record", records);
 
 // start the Express server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
