66057a05a77b601f84d8cf143957a7440cb5902b
surfiniaburger <jdmasciano2@gmail.com>
2024-10-14 19:06:10 +0100

    revisited port 5050

--- mern/server/server.js
+++ mern/server/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import records from "./routes/record.js";
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
