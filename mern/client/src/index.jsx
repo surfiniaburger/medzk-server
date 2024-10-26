dd17559b525e76cd78324cf780ef93271a790f72
surfiniaburger <jdmasciano2@gmail.com>
2024-10-26 09:23:26 +0100

    auth client

---
+++ mern/client/src/index.jsx
@@ -0,0 +1,24 @@
+// src/index.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+      audience="YOUR_API_IDENTIFIER" // Optional: If using Auth0 for API authorization
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
+  </React.StrictMode>
+);
