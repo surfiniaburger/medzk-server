1811fba69780f5958fbc05d5372e27ca6b6c8596
surfiniaburger <jdmasciano2@gmail.com>
2024-10-30 12:00:16 +0100

    render vite

--- mern/client/src/App.jsx
+++ mern/client/src/App.jsx
@@ -1,8 +1,9 @@
+import Vision from "./components/vision";
 
 const App = () => {
   return (
     <div className="w-full p-6">
-      <h1>Client logic as been moved to https://upgraded-palm-tree.vercel.app/</h1>
+      <Vision/>
     </div>
   );
 };
