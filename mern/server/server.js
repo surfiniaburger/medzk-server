27a2d7067d8450f54eeb78a2535efac087374c3c
surfiniaburger <jdmasciano2@gmail.com>
2024-11-11 08:28:18 +0100

    Google maps api key

--- mern/server/server.js
+++ mern/server/server.js
@@ -7,24 +7,28 @@ import path from 'path';
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-// Handle Google Credentials Setup
-const googleCredentials = process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON;
-if (googleCredentials) {
-  const credentialsPath = path.resolve('./gem-creation.json');
-  fs.writeFileSync(credentialsPath, googleCredentials);
+// Handle Google Credentials Setup conditionally based on environment
+if (process.env.NODE_ENV === 'production') {
+  const googleCredentials = process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON;
+  if (googleCredentials) {
+    const credentialsPath = path.resolve('./gem-creation.json');
+    fs.writeFileSync(credentialsPath, googleCredentials);
 
-  // Set the GOOGLE_APPLICATION_CREDENTIALS environment variable
-  process.env.GOOGLE_APPLICATION_CREDENTIALS = credentialsPath;
+    // Set the GOOGLE_APPLICATION_CREDENTIALS environment variable
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = credentialsPath;
+  } else {
+    console.error("GOOGLE_APPLICATION_CREDENTIALS_JSON environment variable is missing!");
+    process.exit(1);
+  }
 } else {
-  console.error("GOOGLE_APPLICATION_CREDENTIALS_JSON environment variable is missing!");
-  process.exit(1);
+  console.log("Skipping Google Credentials setup in development environment.");
 }
 
 // Configure CORS
 app.use(cors({
   origin: (origin, callback) => {
     const allowedOrigins = [
-      'http://localhost:3000/',
+      'http://localhost:3000',
       'https://medzk-server.vercel.app',
       'https://medzk-server.onrender.com',
       
